9c497c20df3ff2b4f9ca99d8f9e4daf44ec76f75
Supremo1337 <lucaswyllame12@gmail.com>
2022-07-21 21:42:28 -0300

    feature(home): create more icons for latBar

--- components/LeftBar/index.js
+++ components/LeftBar/index.js
@@ -11,12 +11,19 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import TopBar from "./../TopBar/index";
 import { useStyles } from "../../pages/HomePage";
 import HomeIcon from "@mui/icons-material/Home";
-import ExploreIcon from '@mui/icons-material/Explore';
+import ExploreIcon from "@mui/icons-material/Explore";
+import SlowMotionVideoIcon from "@mui/icons-material/SlowMotionVideo";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import HistoryIcon from "@mui/icons-material/History";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ContentCutIcon from "@mui/icons-material/ContentCut";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 
 const drawerWidth = 240;
 
@@ -65,30 +72,105 @@ export default function ClippedDrawer() {
             <ListItem disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  <ExploreIcon />
+                  <SlowMotionVideoIcon />
                 </ListItemIcon>
                 <ListItemText
                   classes={{ primary: classes.listItemText }}
-                  primary={"Explorar"}
+                  primary={"Shorts"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SubscriptionsIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Inscrições"}
                 />
               </ListItemButton>
             </ListItem>
           </List>
           <Divider />
           <List>
-            {["All mail", "Trash", "Spam"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText
-                    classes={{ primary: classes.listItemText }}
-                    primary={text}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <VideoLibraryIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Biblioteca"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HistoryIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Hístorico"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SlideshowIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Seus vídeos"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <AccessTimeIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Assistir mais tarde"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <ContentCutIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Seus clipes"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <ThumbUpOffAltIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Vídeos marcados como gostei"}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PlaylistAddIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={"Criar playlist"}
+                />
+              </ListItemButton>
+            </ListItem>
           </List>
         </Box>
       </Drawer>
